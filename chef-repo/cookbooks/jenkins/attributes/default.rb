63c9fcab1fea1b74f02149f971b0d4534db6bf5c
KD <kuldeeparyadotcom@gmail.com>
2016-05-15 17:37:57 -0500

    jenkins cookbook deleted as it does not sound in shape

--- chef-repo/cookbooks/jenkins/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-node.default['jenkins']['jenkins_container_name']='myjenkins'
-node.default['jenkins']['slave_agent_port']=50000
-node.default['jenkins']['admine_console_port']=8080
-node.default['jenkins']['jenkins_home_mount_point']='/home/ec2-user/jenkins_home'
--- chef-repo/cookbooks/jenkins/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: jenkins
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
-
-#Install Docker
-include_recipe 'docker::install_docker'
-include_recipe 'jenkins::install_jenkins'
