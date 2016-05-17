cd9d756b7a54912d4360fa0ba27e59e6b6963a5b
KD <kuldeepayradotcom@gmail.com>
2016-05-17 00:42:57 -0500

    Fixing git sync path

--- chef-repo/cookbooks/souls4society/recipes/deploy.rb
+++ chef-repo/cookbooks/souls4society/recipes/deploy.rb
@@ -14,7 +14,7 @@
 
 cloned_dir = File.join(node.default['souls4society']['user_home'], node.default['souls4society']['app_name'])
 
-git node.default['souls4society']['user_home'] do
+git node.default['souls4society']['user_home']['app_name'] do
   repository node.default['souls4society']['repository']
   revision node.default['souls4society']['revision']
   action :sync
@@ -24,6 +24,8 @@ bash 'copy_directory_content' do
   cwd ::File.dirname(node.default['souls4society']['user_home'])
   code <<-EOH
     cp -R "#{cloned_dir}"/* "#{node.default['souls4society']['com']}"
+    cp -R "#{cloned_dir}"/* "#{node.default['souls4society']['org']}"
+    cp -R "#{cloned_dir}"/* "#{node.default['souls4society']['info']}"
+    cp -R "#{cloned_dir}"/* "#{node.default['souls4society']['net']}"
   EOH
 end
-
